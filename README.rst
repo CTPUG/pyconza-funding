ad2102136cb9f7fac120025b1092e68ac894e585
Neil Muller <drnlmuller@gmail.com>
2019-10-12 16:04:17 +0200

    Add setup.py and change to using README.rst

---
+++ README.rst
@@ -0,0 +1,21 @@
+PyConZA Funding application form
+================================
+
+Intended to integrate with `wafer`_.
+
+.. _wavfer: (https://github.com/CTPUG/wafer).
+
+About
+=====
+
+This is a funding application that can be used with wafer. It is fairly minimal
+and specific to how PyCon ZA handles funding, but can be adapted for other
+conferences.
+
+Installing
+==========
+
+This is not a standalone application, but intended to extend wafer.
+
+1. Add pyconza-funding to the requirements.txt file of the conference application.
+2. Add funding to the ``INSTALLED_APPS`` of your site, before any of the ``wafer`` applications.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import sys
+
+from setuptools import find_packages, setup
+
+REQUIRES = [
+    'wafer >= 0.7.0',
+]
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="pyconza_funding",
+    version="0.9,1",
+    url='http://github.com/CTPUG/pyconza-funding',
+    license='ISC',
+    description="A Funding application for PyCon ZA for use with wafer.",
+    long_description=long_description,
+    author='CTPUG',
+    author_email='ctpug@googlegroups.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=REQUIRES,
+    setup_requires=[
+        # Add setuptools-git, so we get correct behaviour for
+        # include_package_data
+        'setuptools_git >= 1.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
