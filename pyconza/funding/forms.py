f2937ab8b9e0a3650e872f535991c315f2b0a4c1
Neil Muller <drnlmuller@gmail.com>
2019-10-13 13:06:41 +0200

    Add javascript totalling of total budget to form

--- pyconza/funding/forms.py
+++ pyconza/funding/forms.py
@@ -10,12 +10,14 @@ from django.utils.translation import ugettext as _
 
 from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, HTML
+from crispy_forms.layout import Layout, Fieldset, Field, Submit, HTML
 from easy_select2.widgets import Select2Multiple
 from markitup.widgets import MarkItUpWidget
 
 from .models import FundingApplication
 
+BUDGET_CLASS = "budget"
+REQUEST_CLASS = "request"
 
 class FundingApplicationForm(forms.ModelForm):
 
@@ -34,6 +36,26 @@ class FundingApplicationForm(forms.ModelForm):
 
         self.helper = FormHelper(self)
         self.helper.include_media = False
+        # We group the budget items together, and add a css class so we can use
+        # javascript for the total
+        # Likewise, we add a css_class to the own_contribution field for javascript
+        # calculation of the total_requested
+        self.helper.layout = Layout(
+            'motivation'
+            'country',
+            Fieldset(
+                'Your budget',
+                Field('travel_amount', css_class=BUDGET_CLASS, onblur="updateTotal()"),
+                Field('accomodation_amount', css_class=BUDGET_CLASS, onblur="updateTotal()"),
+                Field('food_amount', css_class=BUDGET_CLASS, onblur="updateTotal()"),
+                Field('local_transport_amount', css_class=BUDGET_CLASS, onblur="updateTotal()"),
+                Field('other_expenses', css_class=BUDGET_CLASS, onblur="updateTotal()"),
+                HTML('<p>Total Budget: <strong class="%s_total">R </strong></p>' % BUDGET_CLASS),
+                'budget_description',
+            ),
+            Field('own_contribution', css_class=REQUEST_CLASS, onblur="updateRequestedTotal"),
+            HTML('<p>Total Requested: <strong class="%s_total">R </strong></p>' % REQUEST_CLASS),
+        )
         instance = kwargs['instance']
         submit_button = Submit('submit', _('Save') if instance else _('Submit'))
         if instance:
